cecca4b94cf20e6f16e3f06afbf3e918f4f05781
Jesse Raymond <94083757+Jesserc@users.noreply.github.com>
2022-08-17 19:48:12 +0100

    Update SwapToken.sol

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenSwap {
+
+    
     IERC20 token1;
     IERC20 token2;
     event TokenSwapped(string swapped);
@@ -21,12 +23,16 @@ contract TokenSwap {
     // }
 
     //swap function
+    //with my logic, both swappers would have to approve the Swap Contract with openzeppelin "approve" function before a swap can take place.
     function swapToken(
         address owner1,
         address owner2,
         uint256 amount1,
         uint256 amount2
     ) external {
+    //here i'm running a check if both swappers have called the ERC20 approve function on my contract 
+    //and if the allowance from the approve function is >= to the amount specified
+    
         require(
             token1.allowance(owner1, address(this)) >= amount1,
             "Allowance for token one too low"
